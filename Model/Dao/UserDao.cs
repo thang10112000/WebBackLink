3190db208034bdb9fd51a6250c6ed15e5545801a
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2021-10-06 20:11:46 +0700

    kết nối dtb, phân vùng
    kết nối với dtb , phân vùng admin

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        ClickShopDbConText db = null;
+        public UserDao()
+        {
+            db = new ClickShopDbConText();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        //lấy user dựa vào user id
+        public User GetById(string userName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+        public int Login(string userName, string passWord)
+        {
+            var res = db.Users.SingleOrDefault(x => x.UserName == userName);
+            if (res == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (res.Status == false)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (res.Password == passWord)
+                        return 1;
+                    else
+                        return -2;
+                }
+            }
+        }
+    }
+}
---
+++ Model/EF/ClickShopDbConText.cs
@@ -0,0 +1,151 @@
+namespace Model.EF
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ClickShopDbConText : DbContext
+    {
+        public ClickShopDbConText()
+            : base("name=ClickShopDbConText")
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Contact> Contacts { get; set; }
+        public virtual DbSet<Content> Contents { get; set; }
+        public virtual DbSet<Feedback> Feedbacks { get; set; }
+        public virtual DbSet<Menu> Menus { get; set; }
+        public virtual DbSet<MenuType> MenuTypes { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductCategory> ProductCategories { get; set; }
+        public virtual DbSet<Slide> Slides { get; set; }
+        public virtual DbSet<SystemConfig> SystemConfigs { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .Property(e => e.MetaTitle)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Category>()
+                .Property(e => e.CreateBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Category>()
+                .Property(e => e.ModifiedBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Category>()
+                .Property(e => e.MetaDescriptions)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Content>()
+                .Property(e => e.MetaTitle)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Content>()
+                .Property(e => e.CreateBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Content>()
+                .Property(e => e.ModifiedBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Content>()
+                .Property(e => e.MetaDescriptions)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Feedback>()
+                .Property(e => e.Status)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Code)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.MetaTitle)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Price)
+                .HasPrecision(18, 0);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.PromotionPrice)
+                .HasPrecision(18, 0);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.CreateBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.ModifiedBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.MetaDescriptions)
+                .IsFixedLength();
+
+            modelBuilder.Entity<ProductCategory>()
+                .Property(e => e.MetaTitle)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ProductCategory>()
+                .Property(e => e.CreateBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ProductCategory>()
+                .Property(e => e.ModifiedBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ProductCategory>()
+                .Property(e => e.MetaDescriptions)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Slide>()
+                .Property(e => e.CreateBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Slide>()
+                .Property(e => e.ModifiedBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Slide>()
+                .Property(e => e.MetaDescriptions)
+                .IsFixedLength();
+
+            modelBuilder.Entity<SystemConfig>()
+                .Property(e => e.ID)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Tag>()
+                .Property(e => e.ID)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.UserName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.CreateBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.ModifiedBy)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.MetaDescriptions)
+                .IsFixedLength();
+        }
+    }
+}
