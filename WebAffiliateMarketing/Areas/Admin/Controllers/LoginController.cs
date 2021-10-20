68cb62a32b792769340539b48da4bf7196b12d5a
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2021-10-20 08:45:50 +0700

    đăng xuất

--- WebAffiliateMarketing/Areas/Admin/Controllers/LoginController.cs
+++ WebAffiliateMarketing/Areas/Admin/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using WebAffiliateMarketing.Areas.Admin.Models;
 using WebAffiliateMarketing.Common;
 
@@ -48,6 +49,11 @@ namespace WebAffiliateMarketing.Areas.Admin.Controllers
                 }
             }
             return View("Index");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
         }
 	}
 }
