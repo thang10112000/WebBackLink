66e3dfc8bfa52d96b4481bf42d6a882aa207d5a4
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2021-10-25 20:47:07 +0700

    Đọc dữ liệu cho các thành phần dùng chung

---
+++ Model/Dao/MenuDao.cs
@@ -0,0 +1,22 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+  public  class MenuDao
+    {
+        ClickShopDbContext db = null;
+        public MenuDao()
+        {
+            db = new ClickShopDbContext();
+        }
+        public List<Menu> ListByGroupId(int groupId)
+        {
+            return db.Menus.Where(x => x.TypeID == groupId && x.Status == true).OrderBy(x => x.DisplayOrder).ToList();
+        }
+    }
+}
--- WebAffiliateMarketing/Controllers/HomeController.cs
+++ WebAffiliateMarketing/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Dao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,29 @@ namespace WebAffiliateMarketing.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [ChildActionOnly]
+        [OutputCache(Duration = 3600 * 24)]
+        public ActionResult MainMenu()
+        {
+            var model = new MenuDao().ListByGroupId(1);
+            return PartialView(model);
+        }
+
+        [ChildActionOnly]
+        [OutputCache(Duration = 3600 * 24)]
+        public ActionResult TopMenu()
+        {
+            var model = new MenuDao().ListByGroupId(2);
+            return PartialView(model);
+        }
+
+        [ChildActionOnly]
+        [OutputCache(Duration = 3600 * 24)]
+        public ActionResult Footer()
+        {
+            var model = new FooterDao().GetFooter();
+            return PartialView(model);
         }
     }
 }
