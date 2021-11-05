51e2edde8b7c98996d05cfd6cee7ca756703bdd1
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2021-11-05 20:27:07 +0700

    update sql

---
+++ Model/EF/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿namespace Models.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("OrderDetail")]
+    public partial class OrderDetail
+    {
+        public long ProductID { get; set; }
+
+        [Key]
+        public long OrderID { get; set; }
+
+        public decimal? Price { get; set; }
+    }
+}
