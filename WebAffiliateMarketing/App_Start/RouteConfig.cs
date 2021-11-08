b06df50409a6493894563752e695fad3ccf55cd4
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2021-11-08 21:04:12 +0700

    tính năng yêu thích
    nhấn yêu thích sẽ thêm vào giỏ hàng

--- WebAffiliateMarketing/App_Start/RouteConfig.cs
+++ WebAffiliateMarketing/App_Start/RouteConfig.cs
@@ -32,6 +32,12 @@ namespace WebAffiliateMarketing
             namespaces: new[] { "OnlineShop.Controllers" }
         );
             routes.MapRoute(
+       name: "Add Cart",
+       url: "them-gio-hang",
+       defaults: new { controller = "Cart", action = "AddItem", id = UrlParameter.Optional },
+       namespaces: new[] { "WebAffiliateMarketing.Controllers" }
+   );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }, 
---
+++ WebAffiliateMarketing/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using Model.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAffiliateMarketing.Models;
+
+namespace WebAffiliateMarketing.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        private const string CartSession = "CartSession";
+        public ActionResult Index()
+        {
+
+            var cart = Session[CartSession];
+            var list = new List<CartItem>();
+            if (cart != null)
+            {
+                list = (List<CartItem>)cart;
+            }
+            return View(list);
+        }
+        public ActionResult AddItem(long productId, int quantity)
+        {
+            var product = new ProductDao().ViewDetail(productId);
+            var cart = Session[CartSession];
+            if (cart != null)
+            {
+                var list = (List<CartItem>)cart;
+                if (list.Exists(x => x.Product.ID == productId))
+                {
+
+                    foreach (var item in list)
+                    {
+                        if (item.Product.ID == productId)
+                        {
+                            item.Quantity += quantity;
+                        }
+                    }
+                }
+                else
+                {
+                    //tạo mới đối tượng cart item
+                    var item = new CartItem();
+                    item.Product = product;
+                    item.Quantity = quantity;
+                    list.Add(item);
+                }
+                //Gán vào session
+                Session[CartSession] = list;
+            }
+            else
+            {
+                //tạo mới đối tượng cart item
+                var item = new CartItem();
+                item.Product = product;
+                item.Quantity = quantity;
+                var list = new List<CartItem>();
+                list.Add(item);
+                //Gán vào session
+                Session[CartSession] = list;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
