71b818f1c35d9017ddf83d7b4292c29e20db8b22
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2021-12-04 09:37:18 +0700

    up
    up

--- WebAffiliateMarketing/Controllers/UserController.cs
+++ WebAffiliateMarketing/Controllers/UserController.cs
@@ -1,9 +1,10 @@
 ﻿using BotDetect.Web.UI.Mvc;
-
+using Facebook;
 using Model.Dao;
 using Model.EF;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +15,17 @@ namespace WebAffiliateMarketing.Controllers
 {
     public class UserController : Controller
     {
+        private Uri RedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("FacebookCallback");
+                return uriBuilder.Uri;
+            }
+        }
         // GET: User
         [HttpGet]
         public ActionResult Register()
@@ -23,6 +35,61 @@ namespace WebAffiliateMarketing.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        public ActionResult LoginFacebook()
+        {
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = ConfigurationManager.AppSettings["FbAppId"],
+                client_secret = ConfigurationManager.AppSettings["FbAppSecret"],
+                redirect_uri = RedirectUri.AbsoluteUri,
+                response_type = "code",
+                scope = "email",
+            });
+
+            return Redirect(loginUrl.AbsoluteUri);
+        }
+        public ActionResult FacebookCallback(string code)
+        {
+            var fb = new FacebookClient();
+            dynamic result = fb.Post("oauth/access_token", new
+            {
+                client_id = ConfigurationManager.AppSettings["FbAppId"],
+                client_secret = ConfigurationManager.AppSettings["FbAppSecret"],
+                redirect_uri = RedirectUri.AbsoluteUri,
+                code = code
+            });
+
+
+            var accessToken = result.access_token;
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                fb.AccessToken = accessToken;
+                // Get the user's information, like email, first name, middle name etc
+                dynamic me = fb.Get("me?fields=first_name,middle_name,last_name,id,email");
+                string email = me.email;
+                string userName = me.email;
+                string firstname = me.first_name;
+                string middlename = me.middle_name;
+                string lastname = me.last_name;
+
+                var user = new User();
+                user.Email = email;
+                user.UserName = email;
+                user.Status = true;
+                user.Name = firstname + " " + middlename + " " + lastname;
+                user.CreateDate = DateTime.Now;
+                var resultInsert = new UserDao().InsertForFacebook(user);
+                if (resultInsert > 0)
+                {
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserID = user.ID;
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                }
+            }
+            return Redirect("/");
         }
         public ActionResult Logout()
         {
